db248ffbd84a0a170b5d07b675d9ba681c69aedc
Haoyu92 <zhanghaoyu1006@gmail.com>
2017-04-06 15:44:10 -0500

    SQLite practice

---
+++ app/src/main/java/com/instinctcoder/sqlitedb/Student.java
@@ -0,0 +1,16 @@
+package com.instinctcoder.sqlitedb;
+
+public class Student {
+
+    public static final String TABLE = "Student";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_name = "name";
+    public static final String KEY_email = "email";
+    public static final String KEY_age = "age";
+
+    public int student_ID;
+    public String name;
+    public String email;
+    public int age;
+}
